83efbfc7a042dcb7a6e001e3509c3b9b9529a664
Shruti Guha <153054268+TuliShruti@users.noreply.github.com>
2025-05-13 07:45:23 +0530

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 def error_message_detail(error, error_detail: sys):
     """
@@ -20,4 +21,6 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+    
+  
     
