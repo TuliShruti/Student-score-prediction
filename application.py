1ad89755d96ff896f7efd2d2b7a3c1dcccbfe283
Shruti Guha <153054268+TuliShruti@users.noreply.github.com>
2025-05-21 23:53:09 +0530

    docker file created

--- application.py
+++ application.py
@@ -32,4 +32,4 @@ def predict():
         return render_template('home.html')
     
 if __name__=="__main__":
-    application.run(host='0.0.0.0', port=5000)
+    application.run(debug=True, host='0.0.0.0', port=5000)
